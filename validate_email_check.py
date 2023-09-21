32fb5f9bb251946ead1c6e44eccd2ef3ce967f05
Avinandan Panda <105408004+Avinandan24@users.noreply.github.com>
2023-09-21 11:31:35 +0530

    validate_email_check.py

--- validate_email_check.py
+++ validate_email_check.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 def validate_email():
     email = email_entry.get()
@@ -22,35 +23,53 @@ def validate_email():
                             d = 1
                     if k == 1 or j == 1 or d == 1:
                         result_label.config(text="Wrong Email")
+                        green_label.place_forget()
+                        red_label.place(x=180, y=260)
                     else:
                         result_label.config(text="Correct Email")
+                        red_label.place_forget()
+                        green_label.place(x=180, y=260)
                 else:
                     result_label.config(text="Wrong Email")
+                    green_label.place_forget()
+                    red_label.place(x=180, y=260)
             else:
                 result_label.config(text="Wrong Email")
+                green_label.place_forget()
+                red_label.place(x=180, y=260)
         else:
             result_label.config(text="Wrong Email")
+            green_label.place_forget()
+            red_label.place(x=180, y=260)
     else:
         result_label.config(text="Wrong Email")
-
+        green_label.place_forget()
+        red_label.place(x=180, y=260)
 
 window = tk.Tk()
 window.title("Email Validator")
 
-window.geometry("400x200")
+window.geometry("400x350")
 
-email_label = tk.Label(window, text="Enter Your E-mail:")
+email_label = ttk.Label(window, text="Enter Your E-mail:")
 email_label.pack(pady=(20,0))
 
-email_entry = tk.Entry(window, width=30)
+email_entry = ttk.Entry(window, width=30)
 email_entry.pack(pady=(0,10))
 
-validate_button = tk.Button(window, text="Validate Email", command=validate_email)
-validate_button.pack()
+button_frame = ttk.Frame(window)
+button_frame.pack(pady=(0,10))
 
-result_label = tk.Label(window, text="", font=("Helvetica", 16))
+validate_button = ttk.Button(button_frame, text="Validate Email", command=validate_email, style='TButton')
+validate_button.pack(ipadx=10, ipady=5)
+
+result_label = ttk.Label(window, text="", font=("Helvetica", 16))
 result_label.pack(pady=(10,20))
 
+green_label = ttk.Label(window, text="✓", font=("Helvetica", 24), foreground="green")
+red_label = ttk.Label(window, text="✗", font=("Helvetica", 24), foreground="red")
+
+green_label.place(x=180, y=260)
 
 window.update_idletasks()
 width = window.winfo_width()
@@ -58,6 +77,5 @@ height = window.winfo_height()
 x = (window.winfo_screenwidth() // 2) - (width // 2)
 y = (window.winfo_screenheight() // 2) - (height // 2)
 window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-
 
 window.mainloop()
